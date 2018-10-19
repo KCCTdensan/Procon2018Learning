378d6e0170e062dc9c3d96915ae7568fda3c7ff0
RemonJuice <31856897+RemonJuice@users.noreply.github.com>
2018-10-20 00:13:21 +0900

    struct log作成

--- Procon2018Learning/BattleField.hpp
+++ Procon2018Learning/BattleField.hpp
@@ -7,7 +7,12 @@
 class battle_field
 {
 	//stage Stage;
-
+	struct log
+	{
+		panels Panels[MaxY][MaxX];
+		agent[NumTeams][stage::NumAgents];
+		int N[9][9];//nullptr‚Í0
+	};
 public:
 	battle_field();
 	~battle_field();
