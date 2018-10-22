46ebc9b7d4a0a261310a9573973edba5f15f14b9
kamejiro <r116309@g.kobe-kosen.ac.jp>
2018-10-22 18:01:14 +0900

    dll alpha版完成

--- MonteCalroTreeSearchAI/Export.cpp
+++ MonteCalroTreeSearchAI/Export.cpp
@@ -4,23 +4,75 @@
 namespace
 {
 	friend_node *CurrentNode[stage::MaxTurns] = {};
+	unsigned char CntTurn;
 }
 
 void __stdcall Init(const char *QRCodeString)
 {
 	CurrentNode[0] = new friend_node(*(new stage(QRCodeString)));
+	CntTurn = 0;
 }
 
-void AI_API Deinit()
+void __stdcall Deinit()
 {
+	if (CurrentNode[0] == nullptr)
+	{
+		return;
+	}
+	delete CurrentNode[0];
 	for (int i = 0; i < stage::MaxTurns; ++i)
 	{
-		if (CurrentNode[i] == nullptr)
-		{
-			continue;
-		}
-		delete CurrentNode[i];
 		CurrentNode[i] = nullptr;
 	}
 }
 
+void __stdcall SetNumTurns(int NumTurns)
+{
+	CurrentNode[CntTurn]->ChangeNumTurns((unsigned char)NumTurns);
+}
+
+void __stdcall BestMove(action_id *ID1, action_id *ID2)
+{
+	int Result[ID_MaxID][ID_MaxID];
+
+	CurrentNode[CntTurn]->Search(node::NumCallPlay);
+	CurrentNode[CntTurn]->Result(Result);
+	int Max = 0;
+	for (action_id i = 0; i < ID_MaxID; ++i)
+	{
+		for (action_id j = 0; j < ID_MaxID; ++j)
+		{
+			if (Max < Result[i][j])
+			{
+				Max = Result[i][j];
+				*ID1 = i;
+				*ID2 = j;
+			}
+		}
+	}
+
+	CurrentNode[CntTurn]->ChildNode(*ID1, *ID2)->Search(node::NumCallPlay);
+	CurrentNode[CntTurn]->ChildNode(*ID1, *ID2)->Result(Result);
+	Max = 0;
+	for (action_id i = 0; i < ID_MaxID; ++i)
+	{
+		for (action_id j = 0; j < ID_MaxID; ++j)
+		{
+			if (Max < Result[i][j])
+			{
+				Max = Result[i][j];
+				*ID1 = i;
+				*ID2 = j;
+			}
+		}
+	}
+}
+
+void __stdcall Move(action_id FriendID1, action_id FriendID2, action_id OpponentID1, action_id OpponentID2)
+{
+	action_id IDs[][stage::NumAgents] = {
+		{FriendID1, FriendID2}, {OpponentID1, OpponentID2}
+	};
+	CurrentNode[CntTurn + 1] = CurrentNode[CntTurn]->NextNode(IDs);
+	CntTurn++;
+}
--- MonteCalroTreeSearchAI/Export.h
+++ MonteCalroTreeSearchAI/Export.h
@@ -3,8 +3,9 @@
 #include "Game.hpp"
 #include "Node.hpp"
 
-#define AI_API	__stdcall __declspec(dllexport)
 
-
-void AI_API Init(const char *QRCodeString);
-void AI_API Deinit();
+__declspec(dllexport) void __stdcall Init(const char *QRCodeString);
+__declspec(dllexport) void __stdcall Deinit();
+__declspec(dllexport) void __stdcall SetNumTurns(int NumTurns);
+__declspec(dllexport) void __stdcall BestMove(action_id *ID1, action_id *ID2);
+__declspec(dllexport) void __stdcall Move(action_id FriendID1, action_id FriendID2, action_id OpponentID1, action_id OpponentID2);
