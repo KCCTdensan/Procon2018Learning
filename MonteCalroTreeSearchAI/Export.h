13b6d4ba69a9329b44e6456516cdfd74e8fc59ff
kamejiro <r116309@g.kobe-kosen.ac.jp>
2018-10-24 12:52:24 +0900

    dll export

--- MonteCalroTreeSearchAI/Export.h
+++ MonteCalroTreeSearchAI/Export.h
@@ -4,8 +4,8 @@
 #include "Node.hpp"
 
 
-__declspec(dllexport) void __stdcall Init(const char *QRCodeString);
-__declspec(dllexport) void __stdcall Deinit();
-__declspec(dllexport) void __stdcall SetNumTurns(int NumTurns);
-__declspec(dllexport) void __stdcall BestMove(action_id *ID1, action_id *ID2);
-__declspec(dllexport) void __stdcall Move(action_id FriendID1, action_id FriendID2, action_id OpponentID1, action_id OpponentID2);
+extern "C" void __stdcall Init(const char *QRCodeString);
+extern "C" void __stdcall Deinit();
+extern "C" void __stdcall SetNumTurns(int NumTurns);
+extern "C" void __stdcall BestMove(action_id *ID1, action_id *ID2);
+extern "C" void __stdcall Move(action_id FriendID1, action_id FriendID2, action_id OpponentID1, action_id OpponentID2);
