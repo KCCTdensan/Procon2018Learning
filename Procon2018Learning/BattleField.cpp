218cf4299656f54cc2864462a918467dcdbf6036
実 石原 <sahara.yuma@gmail.com>
2018-10-27 08:47:34 +0900

    カード表示/カード入力

--- Procon2018Learning/BattleField.cpp
+++ Procon2018Learning/BattleField.cpp
@@ -55,9 +55,9 @@ void battle_field::Battle(int NumTurn)
 			intention Intention1P_2 = IntentionIDs[Team_1P][1];
 			//CurrentNode->PrintChildNodeInfo();
 			cout << "1P-1  x : " << (int)Intention1P_1.DeltaX << " y : " << (int)Intention1P_1.DeltaY << endl;
-			cout << "ID:[ " << (int)IntentionIDs[Team_1P][0] << " ]" << endl;
+			cout << "ID:[ " << intoCard((int)IntentionIDs[Team_1P][0]) << " ]" << endl;
 			cout << "1P-2  x : " << (int)Intention1P_2.DeltaX << " y : " << (int)Intention1P_2.DeltaY << endl;
-			cout << "ID:[ " << (int)IntentionIDs[Team_1P][1] << " ]" << endl;
+			cout << "ID:[ " << intoCard((int)IntentionIDs[Team_1P][1]) << " ]" << endl;
 		}
 		if (CurrentNode == nullptr) { cout << "null1" << endl; }
 		if (CurrentNode->ChildNode(IntentionIDs[Team_1P]) == nullptr) { cout << "null2" << endl; }
@@ -82,15 +82,15 @@ void battle_field::Battle(int NumTurn)
 			intention Intention2P_2 = IntentionIDs[Team_2P][1];
 			//CurrentNode->ChildNode(IntentionIDs[Team_1P])->PrintChildNodeInfo();
 			cout << "2P-1 : x : " << (int)Intention2P_1.DeltaX << " y : " << (int)Intention2P_1.DeltaY << endl;
-			cout << "ID: [" << (int)IntentionIDs[Team_2P][0] << " ]" << endl;
+			cout << "ID: [" << intoCard((int)IntentionIDs[Team_2P][0]) << " ]" << endl;
 			cout << "2P-2 : x : " << (int)Intention2P_2.DeltaX << " y : " << (int)Intention2P_2.DeltaY << endl;
-			cout << "ID: [" << (int)IntentionIDs[Team_2P][1] << " ]" << endl;
+			cout << "ID: [" << intoCard((int)IntentionIDs[Team_2P][1]) << " ]" << endl;
 		}
 		
 		cout << "=====================Input intentionIDs =====================" << endl;
+		cout << "7 8 9" << endl;
+		cout << "4 5 6" << endl;
 		cout << "1 2 3" << endl;
-		cout << "4 0 5" << endl;
-		cout << "6 7 8" << endl;
 		bool Results[NumTeams][stage::NumAgents];
 		for (int i  = 0; i < NumTeams; i++)
 		{
@@ -101,7 +101,7 @@ void battle_field::Battle(int NumTurn)
 				int UserIntention;
 				cout << "Agent" << j+1 << ":" << endl;
 				cin >> UserIntention;
-				UserIntentions[(i == 0) ? Team_1P : Team_2P][j] = (action_id)UserIntention;
+				UserIntentions[(i == 0) ? Team_1P : Team_2P][j] = (action_id)intoID(UserIntention);
 			}
 		}
 		cout << "=====================End Input intentionIDs =====================" << endl;
@@ -149,4 +149,53 @@ void battle_field::Battle(int NumTurn)
 	} while (isContinue != '0');
 	delete CurrentNode;
 	CurrentNode = nullptr;
+}
+
+int battle_field::intoCard(action_id _IntentinoID)
+{
+	switch (_IntentinoID)
+	{
+	case 0: return 5;
+			break;
+	case 1: return 7;
+			break;
+	case 2: return 8;
+			break;
+	case 3: return 9;
+			break;
+	case 4: return 4;
+			break;
+	case 5: return 6;
+			break;
+	case 6: return 1;
+			break;
+	case 7: return 2;
+			break;
+	case 8: return 3;
+			break;
+	}
+}
+int battle_field::intoID(int _Card)
+{
+	switch (_Card)
+	{
+	case 5: return 0;
+			break;
+	case 7: return 1;
+			break;
+	case 8: return 2;
+			break;
+	case 9: return 3;
+			break;
+	case 4: return 4;
+			break;
+	case 6: return 5;
+			break;
+	case 1: return 6;
+			break;
+	case 2: return 7;
+			break;
+	case 3: return 8;
+			break;
+	}
 }
